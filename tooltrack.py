0d11dc87c859b2ae41974a0b7e5e38d366ea075e
akilis12345 <aquillesroviel@gmail.com>
2026-05-07 00:10:06 +0800

    Update tooltrack.py

--- tooltrack.py
+++ tooltrack.py
@@ -32,12 +32,13 @@ if not all([
 # =========================
 # MAIL CONFIG (FIXED)
 # =========================
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
-app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
+app.config['MAIL_USERNAME'] = 'apikey'  # literally the word 'apikey'
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = ('ToolTrack System', 'tooltrack2026@gmail.com')
 
 app.config['MAIL_DEBUG'] = True
 app.config['MAIL_TIMEOUT'] = 10
